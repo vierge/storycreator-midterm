978b9825e311dcf6b5c7a65ad1537c4d0377c8a0
vierge <marancholia@gmail.com>
2020-05-05 13:01:06 +0000

    users table written. here_for_tags is stretch

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -3,5 +3,11 @@
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  username VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  display_name VARCHAR(255),
+  date_joined TIMESTAMP DEFAULT Now(),
+  is_active BOOLEAN DEFAULT true,
+  here_for_tags TEXT ARRAY
 );
+
