ffe4636b909e93b4688bf279d8af79e1fe22b771
vierge <marancholia@gmail.com>
2020-05-06 16:36:05 +0000

    commenting get-posts.js

--- public/scripts/get-posts.js
+++ public/scripts/get-posts.js
@@ -1,4 +1,5 @@
 // setup and connect to database
+// TESTING POOL vvvv
 
 const { Pool } = require('pg');
 const dbParams = require('../../lib/db.js');
@@ -11,12 +12,15 @@ const db = new Pool({
 });
 db.connect();
 
-const args = process.argv.slice[2]
+// TESTING POOL^^^^
 
-const dummyArg = {
-  user: 'vierge',
-  date: '2020-05-05 12:00:00',
-  flags: '-u'
+const args = process.argv.slice[2] // This is for command line testing... remove on deployment
+
+
+const dummyArg = {              // this is the anatomy of a getPost argument
+  user: 'vierge',               // current user's username
+  date: '2020-05-05 12:00:00',  // date of request (i don't know that this is actually necessary)
+  flags: '-u'                   // this is the flag that tells the function what to pull
 }
 
 const getPosts = async function(obj) {
@@ -25,16 +29,16 @@ const getPosts = async function(obj) {
   let condition = '';
 
     switch(flags){
-    case '-u':
+    case '-u': // pull all info for the user's stories and snippets
       condition = `WHERE users1.username = '${user}' OR users2.username = '${user}`;
       break;
-    case '-v':
+    case '-v': // select only completed snippets
       condition = `WHERE snippets.date_completed IS NOT NULL`;
       break;
-    case '-t':
+    case '-s': // search for stories with a name like.....
       condition = `WHERE stories.name LIKE '%May%'`;
       break;
-    default: '';
+    default: ''; // no flag should return the default first view
   };
 
   const query = {
