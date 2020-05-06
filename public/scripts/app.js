7037879c1b4967a4d50105870dfb30c44897b2f1
afairlie <ariane.fairlie@gmail.com>
2020-05-06 23:17:54 +0000

    our first route is functional

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,26 +1,14 @@
-// $(() => {
-//   console.log("what is happening over here?");
-//  getPosts({
-//    user: 'vierge',
-//    date: 'whatever',
-//    flag: '-u'
-//  }).then((res, err) => {
-//    console.log(res);
-//  })
-//  .catch(err => {
-//    console.log(err.stack);
-//  })
-// });
-
+//query params - where: req.info (?) query params express server
 
 $(() => {
   $.ajax({
     method: "GET",
-    url: "/api/database"
-  }).done((users) => {
-    console.log('this is where we need to be!');
-    // for(user of users) {
-    //   $("<div>").text(user.name).appendTo($("body"));
-    // }
-  });;
+    url: "/api/database?info=-u",
+    success: (response) => {
+      console.log(response);
+      console.log('this is where we need to be!');
+    }
+  })
 });
+
+
--- routes/database.js
+++ routes/database.js
@@ -4,14 +4,17 @@ const getPosts = require('../public/scripts/get-posts.js');
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
+    const queryFlag = req.query.info;
+    // console.log(req.query.info);
     console.log('this is where we at!');
     getPosts({
       db: db,
-      flag: "-u"
-    }).then(res => {
-      console.log(res[0]);
-      console.log(res[1]);
-    })
+      flag: queryFlag
+    }).then(data => {
+      const left = data[0].rows;
+      const right = data[0].rows;
+      res.json({ left, right });
+    });
   });
   return router;
 };
