bd54ede7782f8f0cbc89f43816f9dcdf33abb789
afairlie <ariane.fairlie@gmail.com>
2020-05-07 22:54:09 +0000

    clean up client b

--- public/scripts/app-b.js
+++ public/scripts/app-b.js
@@ -1,29 +1,21 @@
 $(() => {
 
-  // $.ajax({
-  //   method: "GET",
-  //   url: "/api/snippets",
-  //   success: (response) => {
-  //     console.log('(api snippets saying hello from app-b');
-  //   }
-  // })
-
   // TESTING NEW SNIPPET, will have to make .create-new-snippet listener
   $('.create-new-story').on('click', function() {
     $('.modal-overlay').show();
     $('#modal-write-snippet').addClass('modal-active');
     })
-    console.log('inside the jquery ready');
-        // const count = $newTweet.find('.counter').val();
+
   const $newSnippet = $('#modal-write-snippet form');
   console.log($newSnippet);
     $newSnippet.submit( function(event) {
       const snippetText = $('#snippet-text').val();
       console.log(snippetText);
 
-      // AJAX REQUEST TO /stories/:id here for SNIPPET
+
       // WE NEED THE STORY ID TO BE LOADED ON RENDER ***
       const storyID = 1;
+      // AJAX REQUEST TO /stories/:id here for SNIPPET
       $.post(`/stories/${storyID}`, {snippetText})
       .then((data) => {
         // ARRAY WITH NEW OBJ
