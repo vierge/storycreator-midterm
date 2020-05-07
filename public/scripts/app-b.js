3bed935b5d193126e4b524633e7193566d220e58
afairlie <ariane.fairlie@gmail.com>
2020-05-07 23:04:45 +0000

    more cleanup

--- public/scripts/app-b.js
+++ public/scripts/app-b.js
@@ -7,24 +7,22 @@ $(() => {
     })
 
   const $newSnippet = $('#modal-write-snippet form');
-  console.log($newSnippet);
-    $newSnippet.submit( function(event) {
-      const snippetText = $('#snippet-text').val();
-      console.log(snippetText);
+
+  $newSnippet.submit( function(event) {
+    const snippetText = $('#snippet-text').val();
+    console.log(snippetText);
 
 
-      // WE NEED THE STORY ID TO BE LOADED ON RENDER ***
-      const storyID = 1;
-      // AJAX REQUEST TO /stories/:id here for SNIPPET
-      $.post(`/stories/${storyID}`, {snippetText})
-      .then((data) => {
-        // ARRAY WITH NEW OBJ
-        console.log(data);
-      })
+    // WE NEED THE STORY ID TO BE LOADED ON RENDER ***
+    const storyID = 1;
+    // AJAX REQUEST TO /stories/:id here for SNIPPET
+    $.post(`/stories/${storyID}`, {snippetText})
+    .then((data) => {
+      // ARRAY WITH OBJ - NEW SNIPPET RETURNED
+      console.log(data);
+    })
 
 
     event.preventDefault();
-    });
-
-  console.log('submit handler already recognized');
+  });
 });
