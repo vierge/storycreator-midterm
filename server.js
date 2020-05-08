3da15ecd5eacb17087c19e857a1434f8a5fc6a06
vierge <marancholia@gmail.com>
2020-05-08 02:26:07 +0000

    faked a login. todo: cookie-session, actual user/password lookup

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const databaseRoutes = require("./routes/database");
+const loginRoutes = require("./routes/login");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -48,6 +49,7 @@ app.use("/api/users", usersRoutes(db));
 // app.use("/api/widgets", widgetsRoutes(db));
 app.use("/api/database", databaseRoutes(db));
 // Note: mount other resources here, using the same pattern above
+app.use("/api/login", loginRoutes(db));
 
 
 // Home page
