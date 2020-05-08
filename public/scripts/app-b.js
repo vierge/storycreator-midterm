6f5b647167aece3ba31198c99d725378c00fa2c4
afairlie <ariane.fairlie@gmail.com>
2020-05-08 00:25:19 +0000

    initial commit, edits with mara

--- public/scripts/app-b.js
+++ public/scripts/app-b.js
@@ -1,29 +1,29 @@
-$(() => {
+// $(() => {
 
-  // TESTING NEW SNIPPET, will have to make .create-new-snippet listener
-  $('.create-new-story').on('click', function() {
-    $('.modal-overlay').show();
-    $('#modal-write-snippet').addClass('modal-active');
-    })
+//   // TESTING NEW SNIPPET, will have to make .create-new-snippet listener
+//   $('.create-new-story').on('click', function() {
+//     $('.modal-overlay').show();
+//     $('#modal-write-snippet').addClass('modal-active');
+//     })
 
-  const $newSnippet = $('#modal-write-snippet form');
+//   const $newSnippet = $('#modal-write-snippet form');
 
-  $newSnippet.submit( function(event) {
-    const snippetText = $('#snippet-text').val();
-    console.log(snippetText);
-
-
-    // WE NEED THE STORY ID TO BE LOADED ON RENDER ***
-    const storyID = 1;
-    // AJAX REQUEST TO /stories/:id here for SNIPPET
-    $.post(`/stories/${storyID}`, {snippetText})
-    .then((data) => {
-      // ARRAY WITH OBJ - NEW SNIPPET RETURNED
-      // createPendingPost(data, '.secondary-container')
-      console.log(data);
-    })
+//   $newSnippet.submit( function(event) {
+//     const snippetText = $('#snippet-text').val();
+//     console.log(snippetText);
 
 
-    event.preventDefault();
-  });
-});
+//     // WE NEED THE STORY ID TO BE LOADED ON RENDER ***
+//     const storyID = 1;
+//     // AJAX REQUEST TO /stories/:id here for SNIPPET
+//     $.post(`/stories/${storyID}`, {snippetText})
+//     .then((data) => {
+//       // ARRAY WITH OBJ - NEW SNIPPET RETURNED
+//       // createPendingPost(data, '.secondary-container')
+//       console.log(data);
+//     })
+
+
+//     event.preventDefault();
+//   });
+// });
